ec9717ead6a2ca6180060902368ff10edd95824d
Oscar-AC <carranzav_oscar@hotmail.com>
2019-12-19 21:30:36 -0600

    Comence a resolver el primer problema

---
+++ Ejercicio_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        //Write a program that reads a text file and prints its odd lines on the
+        //console
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
