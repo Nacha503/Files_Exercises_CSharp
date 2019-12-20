cb69e8e7df1f78c76875367da5229cd0db2506e4
Oscar-AC <carranzav_oscar@hotmail.com>
2019-12-19 22:23:54 -0600

    hacer que imprama las lineas impares!

--- Ejercicio_1/Program.cs
+++ Ejercicio_1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Ejercicio_1
 {
@@ -10,10 +11,59 @@ namespace Ejercicio_1
     {
         //Write a program that reads a text file and prints its odd lines on the
         //console
+        private static string textoPreProcesado;
+        private static StringBuilder textoFinal = new StringBuilder();
+
+
         static void Main(string[] args)
         {
+            string texto = LeerLineasImpares();
+            ImprimirLineasImpares(texto);
+
+            Console.ReadKey();
+
 
 
         }
+
+
+        // no me imprime las lineas impares T_T
+
+        public static string LeerLineasImpares()
+        {
+            int numeroDeLinea = 1;
+            using (StreamReader lectorDeTexto = new StreamReader(@".\LoreIpsum.txt"))
+            {
+                do
+                {
+                    if ((numeroDeLinea % 2) != 0)
+                    {
+                        textoPreProcesado = lectorDeTexto.ReadLine();
+                        textoFinal.Append(textoPreProcesado + "\n");
+                        numeroDeLinea++;
+                    }
+                    else
+                    {
+                        //textoPreProcesado = null;
+                        numeroDeLinea++;
+                    }
+
+
+                } while (textoPreProcesado != null);
+
+            }
+
+            return textoFinal.ToString();
+
+
+        }
+
+        public static void ImprimirLineasImpares(string lineasImpares)
+        {
+            Console.WriteLine(lineasImpares);
+
+        }
+
+
     }
 }
